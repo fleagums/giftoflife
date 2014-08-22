caff94bca33341f11cdc6e84e85ea9741a56261a
vagrant <vagrant@local>
2014-08-22 15:08:13 +0000

    add started working on landing page and styling of the landing page

--- sites/all/themes/bootstrap_giftoflife/template.php
+++ sites/all/themes/bootstrap_giftoflife/template.php
@@ -4,3 +4,10 @@
  * @file
  * template.php
  */
+
+function bootstrap_giftoflife_preprocess_html(&$variables) {
+    drupal_add_css(
+        'http://fonts.googleapis.com/css?family=Josefin+Sans',
+        array('type' => 'external')
+    );
+}
